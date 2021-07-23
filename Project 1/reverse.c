d554fe293b67ab77b5d550ab01d2e283012cbea0
jufrisk <jplayerf.12@gmail.com>
2021-07-23 17:16:53 +0300

    updated reverse.c to latest

--- Project 1/reverse.c
+++ Project 1/reverse.c
@@ -3,59 +3,82 @@
 #include <string.h>
 #include <stdbool.h>
 
+
 void readInput();
 void readFileToScreen();
 void readAndWrite();
 int main(int argc, char *argv[]) {
-    
-    if (strcmp(argv[1], argv[2]) == 0) {
-        fprintf(stderr, "Input and output file must differ\n");
-        exit(1);
-    } else if (argc == 1) {
-        readInput();
+    if (argc == 1) {
+        //readInput();
     } else if (argc == 2) {
         readFileToScreen(argv[1]);
     } else if (argc == 3) {
+        if (strcmp(argv[1],argv[2]) == 0) {
+            fprintf(stderr, "Input and output files must differ.\n");
+            exit(1);
+        } 
         readAndWrite(argv[1], argv[2]);
     } else {
-        printf("usage: reverse <input> <output>\n");
+        printf("usage: reverse <input> <output>");
     }
+    
 }
 
-void readInput() {
-
-}
+// void readInput() {
+//     char *text = NULL;
+//     size_t len = 0;
+//     size_t read;
+//     char *reversed;
+//     while ((read = getline(&text, &len, stdin)) != -1 || len == 1) {
+//         printf("%s", text);
+//         if (reversed == NULL) {
+//             reversed = (char*)malloc(strlen(text)+1);
+//             strcpy(reversed, text);
+//             continue;
+//         } 
+//         reversed = (char*)realloc(reversed, strlen(text)+1);
+//         char *temp = (char*)malloc(strlen(reversed));
+//         strcpy(temp, reversed);
+//         strcpy(reversed, text);
+//         if (reversed[strlen(text)-1] != '\n') {
+//             strcat(reversed, "\n");
+//         }
+        
+//         strcat(reversed, temp);
+//         free(temp);
+//     }
+//     free(text);
+//     fclose(stdin);
+// }
 
 void readFileToScreen(char tiedosto1[]) {
     FILE *fp;
     
     if((fp = fopen(tiedosto1, "r"))==NULL){
-		fprintf(stderr, "error: cannot open file '%s'\n", tiedosto1);
+		printf("error: cannot open file '%s'", tiedosto1);
 		exit(1);
 	}
     
     char *text = NULL;
     char *reversed = NULL;
     size_t len = 0;
-    int i = 0;
-    while (getline(&text, &len, fp) != -1) {
+    char *temp;
+    while ((getline(&text, &len, fp)) != -1) {
+
         if (reversed == NULL) {
-            if ((reversed = (char*)malloc(strlen(text)+1)) == NULL) {
-                fprintf(stderr, "malloc failed\n");
-                exit(1);
-            }
+            reversed = (char*)malloc(strlen(text)+1);
             strcpy(reversed, text);
             continue;
-        }
+        } 
         if ((reversed = (char*)realloc(reversed, strlen(text)+1)) == NULL) {
-            fprintf(stderr, "malloc failed\n");
+            fprintf(stderr, "malloc failed.\n");
             exit(1);
         }
-        char *temp;
         if ((temp = (char*)malloc(strlen(reversed))) == NULL) {
-            fprintf(stderr, "malloc failed\n");
+            fprintf(stderr, "malloc failed.\n");
             exit(1);
         }
+        
         strcpy(temp, reversed);
         strcpy(reversed, text);
         if (reversed[strlen(text)-1] != '\n') {
@@ -66,10 +89,11 @@ void readFileToScreen(char tiedosto1[]) {
         free(temp);
     }
         
-    printf("\n%s", reversed);
+    printf("%s", reversed);
     fclose (fp);
     free(text);
     free(reversed);
+    
     
 }
 
@@ -78,32 +102,27 @@ void readAndWrite(char tiedosto1[], char tiedosto2[]) {
     FILE *fp;
     
     if((fp = fopen(tiedosto1, "r"))==NULL){
-		fprintf(stderr, "error: cannot open file '%s'\n", tiedosto1);
+		printf("error: cannot open file '%s'", tiedosto1);
 		exit(1);
 	}
     
     char *text = NULL;
     char *reversed = NULL;
     size_t len = 0;
-    int i = 0;
+    char *temp;
     while ((getline(&text, &len, fp)) != -1) {
 
         if (reversed == NULL) {
-            if ((reversed = (char*)malloc(strlen(text)+1)) == NULL) {
-                fprintf(stderr, "malloc failed\n");
-                exit(1);
-            }
+            reversed = (char*)malloc(strlen(text)+1);
             strcpy(reversed, text);
             continue;
         } 
         if ((reversed = (char*)realloc(reversed, strlen(text)+1)) == NULL) {
-            fprintf(stderr, "malloc failed\n");
+            fprintf(stderr, "malloc failed.\n");
             exit(1);
         }
-
-        char *temp;
         if ((temp = (char*)malloc(strlen(reversed))) == NULL) {
-            fprintf(stderr, "malloc failed\n");
+            fprintf(stderr, "malloc failed.\n");
             exit(1);
         }
         strcpy(temp, reversed);
@@ -116,11 +135,11 @@ void readAndWrite(char tiedosto1[], char tiedosto2[]) {
         free(temp);
     }
         
-    printf("\n%s", reversed);
+    printf("%s", reversed);
     fclose (fp);
 
     if((fp = fopen(tiedosto2, "w"))==NULL){
-		fprintf(stderr,"error: cannot open file '%s'\n", tiedosto2);
+		printf("error: cannot open file '%s'", tiedosto2);
 		exit(1);
 	}
     fprintf(fp, "%s", reversed);
